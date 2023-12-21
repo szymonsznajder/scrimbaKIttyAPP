c89f1fc33406ef56d0a1fc73780736436168317d
Szymon Sznajder <sznajder.szymon@gmail.com>
2023-12-21 21:24:18 +0000

    some chnages

---
+++ index.js
@@ -0,0 +1,21 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+const appSettings = {
+    databaseURL: "https://scrimbakittyapp-default-rtdb.europe-west1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppingListInDB = ref(database, "shoppingList")
+
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
+    
+    // Challenge: Append a new <li> with text content inputValue to the 'shopping-list' <ul>
+})
